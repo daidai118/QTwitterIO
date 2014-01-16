255a8daf2886d98b5b4ceb64ea927c978ef19890
tsukimizake <shomasd@gmail.com>
2014-01-16 23:20:01 +0900

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -6,13 +6,9 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QNetworkProxyFactory::setUseSystemConfiguration(true);	
 	Tweeter twi("(insert consumer_key here)", "(insert consumer_secret here)"); 
-	if (twi.initialize()){
-		twi.Tweet("test tweet");
-	}else{
-		qDebug()<<"please try again after initialization";
-	}
-
-
+	twi.initialize();
+	twi.Tweet("test tweet");
+	
     return app.exec();
 }
 
