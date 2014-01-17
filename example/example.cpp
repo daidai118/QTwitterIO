46993481ae5f4338d7e9b9afcc8df7b32bcc6cd9
tsukimizake <shomasd@gmail.com>
2014-01-17 20:36:37 +0900

    edit first start behavior

--- example/example.cpp
+++ example/example.cpp
@@ -6,12 +6,18 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QNetworkProxyFactory::setUseSystemConfiguration(true);	
 	Tweeter twi("(insert consumer_key here)", "(insert consumer_secret here)"); 
-+  if (twi.initialize()){
- +    twi.Tweet("test tweet");
- +  }else{
- +    qDebug()<<"please try again after initialization";
- 	return app.exec();
- +  }	
+	if (twi.initialize()){
+		twi.Tweet("test tweet");
+	}else{
+		QWidget *wid=new QWidget();
+		QLabel *label= new QLabel(wid);
+		label->resize(300,100);
+		label->setWordWrap(true);
+		label->setText("Launching OAuth process. please try again after authentification. If your browser doesn't start, please check network connection.");
+		wid->show();
+		return app.exec();
+	}
+
     return app.exec();
 }
 
